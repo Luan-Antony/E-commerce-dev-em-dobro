92d3e8e8cb04579f3a30b6055403cbbb5e7be324
Luan Antony <fplpgluan@gmail.com>
2025-10-01 15:43:16 -0300

    estrutura do header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,26 @@
+import logo from '/logo.png'
+import cart from '/icon-cart.png'
 export const Header = () => {
-    return {
+    return (
+        <header>
+            <img src={logo} alt="Logo" />
 
-    }
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Produtos</a></li>
+                    <li><a href="#">Perguntas frequêntes</a></li>
+                    <li><a href="#">Fale conosco</a></li>
+                </ul>
+
+                <div>
+                    <a>Entrar ou Cadastre-se</a>
+                    <a>
+                        <img src={cart} alt="Cart" />
+                    </a>
+                </div>
+            </nav>
+        </header>
+    )
 }   
---
+++ src/types/image.d.ts
@@ -0,0 +1,4 @@
+declare module 'image.png'
+declare module 'image.jpg'
+declare module 'image.jpeg'
+declare module 'image.svg'
