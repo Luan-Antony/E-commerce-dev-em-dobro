576234e2fc4a673170c7dbf48709e339c9f38f0d
Luan Antony <fplpgluan@gmail.com>
2025-10-16 17:30:52 -0300

    header pronto

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,7 +2,7 @@ import logo from '/logo.png'
 import cart from '/icon-cart.png'
 export const Header = () => {
     return (
-        <header className='border-b border-[#6528D3]'>
+        <header className='border-b border-[#6528D3] py-[1rem]'>
             <section className="max-w-[1450px] m-auto px-8">
                 <div className='flex items-center justify-between'>
                     <img src={logo} alt="Logo" />
@@ -16,8 +16,8 @@ export const Header = () => {
                             <li><a href="#">Fale conosco</a></li>
                         </ul>
                     </nav>
-                    <div className='flex items-center'>
-                        <a>Entrar ou Cadastre-se</a>
+                    <div className='flex items-center gap-[1rem]'>
+                        <a className='px-[2rem] py-[1rem] bg-[#6528D3] rounded-[0.5rem]'>Entrar ou Cadastre-se</a>
                         <a>
                             <img src={cart} alt="Cart" />
                         </a>
