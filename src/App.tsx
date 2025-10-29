7851ab9be32ee9dc723044d66291590d8170e93b
Luan Antony <fplpgluan@gmail.com>
2025-10-29 16:23:29 -0300

    instalando biblioteca do react router dom para criar as rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { Header } from './components/header'
 import { Home } from './pages/Home'
 import './style/globalStyle.css'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <Home />
-    </>
+    </BrowserRouter>
   )
 }
 
